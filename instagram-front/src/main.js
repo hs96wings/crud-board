2212dd81cc8cb4a9677bd0149503c00cf0e96db3
hs96wings <hs96wings@gmail.com>
2025-04-14 00:39:18 +0900

    [Front]: manage에서 영상 추가하기

--- instagram-front/src/main.js
+++ instagram-front/src/main.js
@@ -3,7 +3,23 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
-const app = createApp(App)
+import axios from 'axios'
+
+const app = createApp(App);
+
+axios.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`
+        }
+        return config;
+    },
+    error => {
+        // 인터셉터를 무시하고, 사용자의 본래요청인 화면으로 라우팅
+        return Promise.reject(error);
+    }
+)
 
 app.use(router)
 app.use(vuetify)
--- instagram-front/src/router/index.js
+++ instagram-front/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from "@/views/HomePage.vue"
 import VideoTest from "@/views/VideoTest.vue"
 import LoginPage from "@/views/LoginPage.vue"
+import ManagePage from "@/views/ManagePage.vue"
 
 const routes = [
     { path: '/', name: 'HomePage', component: HomePage },
     { path: '/video', name: 'VideoTest', component: VideoTest },
     { path: '/login', name: 'LoginPage', component: LoginPage },
+    { path: '/manage', name: 'ManagePage', component: ManagePage }
 ]
 
 const router = createRouter({
